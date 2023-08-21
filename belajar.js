e852d946ec9ee4789952d4783b6559114668fa70
Sartunus77 <142812170+Sartunus77@users.noreply.github.com>
2023-08-21 08:23:59 -0700

    Add files via upload

---
+++ belajar.js
@@ -0,0 +1,40 @@
+//cara membuat variabel
+let a = 'a';// variabel yg bisa di ubah
+var b = 'b';
+const c = 'c';// variabel yg tidak bisa di ubah
+
+//variabel bisa menampung tipe data string (text), integen/number, boolean, array, object
+
+let nama = 'moon';//string
+let umur = 21; //number
+let apakahbenar = true;//boolean
+let uang = 5000.0//numbaer namun ada koma
+
+//operator aritmatika
+
+let angka1 = 10
+let angka2 = 40
+console.log(angka1 + angka2);//tambah
+console.log(angka1 - angka2);//kurang 
+console.log(angka1 * angka2);//kalih
+console.log(angka1 / angka2);///bagi
+console.log(angka1 % angka2);///modulus
+
+//srtring bisa di tambah namun tidak bisa di kurang dengan operator aritmatika
+let firstname ='mars';
+let lastname = 'moon';
+let fullname = firstname + lastname;
+console.log(fullname)
+console.log(firstname + ' ' + lastname);
+
+//alur pembacaan code
+let number = 30;
+console.log(number + 20);
+number = 50;
+console.log(number * 2);
+number = false;
+console.log(number + 10);
+number = true;
+console.log(number + 10);
+// jadi false itu artinya 0
+// true itu artinya 1
